43cb7d7b7880f617b20b51a163d3e173e070f59f
Kevin <k.beichler@gmx.com>
2023-11-10 09:38:32 +0100

    added all measuerments function to sensor values class

--- datalogger/sensor-config.h
+++ datalogger/sensor-config.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <sensor.h>
+#include <sensor-values.h>
 #include <string>
 #include <map>
 
@@ -10,6 +11,16 @@ public:
     void addSensor(std::string name, Sensor *sensor)
     {
         _sensors.insert({name, sensor});
+    }
+
+    SensorValues getAllMeasurements()
+    {
+        SensorValues measurements;
+        for (auto [name, sensor]: _sensors)
+        {
+            measurements.addMeasurement(name, sensor->get_temperature());
+        }
+        return measurements;
     }
 
     auto begin()       { return _sensors.begin(); }
