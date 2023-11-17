6d118f6d1e62fa5dbbd6b51bb95a1d1e5242b6f8
Your Name <you@example.com>
2023-11-17 11:13:36 +0100

    added constructor

--- toolcase/pwm_controll.cpp
+++ toolcase/pwm_controll.cpp
@@ -4,21 +4,12 @@ const std::string CONTROLLER_PATH = "/sys/class/pwm/pwmchip0/pwm0";
 #define PERIOD 1000000
 
 
-void LEDDisplay::LEDDisplay_set(int percentage)
+void LEDDisplay::set_percentage(int percentage)
     {
 		if(percentage < 0 || percentage > 100)
 		{
 			throw std::runtime_error("Input has to be between 0 and 100!");
 		}
-		
-		std::ofstream periodFile(CONTROLLER_PATH + "/period");
-		
-		if(!periodFile.is_open())
-		{
-			throw std::runtime_error("Failed to open PWM controller file!");
-		}
-			
-		periodFile << PERIOD;
 		
 		std::ofstream dutyFile(CONTROLLER_PATH + "/duty_cycle");
 		
--- toolcase/pwm_controll.h
+++ toolcase/pwm_controll.h
@@ -10,5 +10,17 @@ class LEDDisplay
     private:
 
     public:
-    void LEDDisplay_set(int percentage);
+    LEDDisplay(std::string controller_path, unsigned int period)
+    {
+        std::ofstream periodFile(controller_path + "/period");
+		
+		if(!periodFile.is_open())
+		{
+			throw std::runtime_error("Failed to open PWM controller file!");
+		}
+			
+		periodFile << period;
+		
+    }
+    void set_percentage(int percentage);
 };
